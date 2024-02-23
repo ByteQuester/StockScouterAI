7b601a118f65c5c30992c1546e62e295a4aa8d61
ByteQuester <151527846+ByteQuester@users.noreply.github.com>
2024-02-23 12:33:14 +0100

    Update main.py

--- app/gallery/apps/read/main.py
+++ app/gallery/apps/read/main.py
@@ -5,6 +5,8 @@ import streamlit as st
 
 def main():
     st.markdown((Path(__file__).parents[2] / "README.md").read_text())
+    st.sidebar.markdown("**Made by** [Mehrdad Touraji](https://www.linkedin.com/in/mehrdad-touraji/)") to be put in first page
+
 
 
 if __name__ == "__main__":
